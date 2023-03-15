0f5f77e641412db877bf6dc5e0a9d228e692b3bb
mchu-1999 <jerry.ee06@nctu.edu.tw>
2023-03-15 16:17:58 +0800

    complete 2 feature

--- main.py
+++ main.py
@@ -3,10 +3,8 @@ from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 from discord import app_commands
-import random
-import config
+import traceback
 import util
-
 
 load_dotenv() 
 token = os.getenv('TOKEN')
@@ -14,39 +12,157 @@ app_id = os.getenv('APP_ID')
 guild_id = os.getenv('GUILD_ID')
 KNS = discord.Object(id=guild_id)
 
-description = '''An example bot to showcase the discord.ext.commands extension module.
-There are a number of utility commands being showcased here.'''
 
-intents = discord.Intents.default()
-intents.members = True
-intents.message_content = True
-intents.guilds = True
+class menuView(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
 
-bot = discord.Client(command_prefix='/', description=description, intents=intents)
-# bot = commands.Bot(command_prefix='/', description=description, intents=intents)
-tree = app_commands.CommandTree(bot)
+    @discord.ui.button(label='跟單收益試算', style=discord.ButtonStyle.green, custom_id='persistent_view:green')
+    async def green(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_modal(simulate())
+
+    @discord.ui.button(label='跟單風險試算', style=discord.ButtonStyle.red, custom_id='persistent_view:red')
+    async def red(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_modal(analyze())
+
+    # @discord.ui.button(label='Grey', style=discord.ButtonStyle.grey, custom_id='persistent_view:grey')
+    # async def grey(self, interaction: discord.Interaction, button: discord.ui.Button):
+    #     await interaction.response.send_message('This is grey.', ephemeral=True)
 
 
-@tree.command(name = "ping", description = "pong!", guild = KNS)
+class simulate(discord.ui.Modal, title='交易員實盤績效查詢&試算'):
+    traderLink = discord.ui.TextInput(
+        label = 'bitget交易員連結',
+        placeholder = '輸入網址'
+    )
+    margin = discord.ui.TextInput(
+        label = '開倉大小',
+        placeholder = '每倉保證金（USDT）'
+    )
+    lossPerPos = discord.ui.TextInput(
+        label = '倉位止損',
+        placeholder = '自訂倉位止損（％）'
+    )
+    month = discord.ui.TextInput(
+        label = '試算時間段',
+        placeholder = '輸入『西元年-月份』ex:2022-09'
+    )
+
+    async def on_submit(self, interaction: discord.Interaction):
+        # print(self.traderLink)
+        resData = await util.copySimulate(str(self.traderLink).split('?id=')[1], int(str(self.margin)), int(str(self.lossPerPos)), str(self.month))
+        # print(resData)
+        # print("交易員：" + resData["traderName"])
+        embed = discord.Embed(
+            title = str(self.month) + "月收益試算",
+            type = "rich",
+            description = f'交易員: {resData["traderName"]}'
+        )\
+        .add_field(name=" ", value=" ", inline=False)\
+        .add_field(name="本金（每倉）", value=f'{ str(self.margin) }USDT')\
+        .add_field(name="倉位", value=f'共{ resData["positionCount"] }倉')\
+        .add_field(name=f"{ str(self.lossPerPos) }％止損", value=f'共觸發{ resData["countSL"] }次')\
+        .add_field(name="總收益（無爆倉）", value=f'{ resData["copyProfit"] }USDT', inline=False)
+        await interaction.response.send_message(embed = embed, ephemeral = True)
+
+    async def on_error(self, interaction: discord.Interaction, error: Exception) -> None:
+        await interaction.response.send_message('Oops! Something went wrong.', ephemeral = True)
+        # Make sure we know what the error actually is
+        traceback.print_exception(type(error), error, error.__traceback__)
+        # send error message to bot log channel
+        botLogChannel = self.get_channel(944846007051620404)
+        botLogChannel.send(f'{type(error)}{error}{error.__traceback__}')
+
+class analyze(discord.ui.Modal, title='交易員風險與回撤試算'):
+    traderLink = discord.ui.TextInput(
+        label = 'bitget交易員連結',
+        placeholder = '輸入網址'
+    )
+    initialCapital = discord.ui.TextInput(
+        label = '本金',
+        placeholder = '跟單資金（USDT）'
+    )
+    maxLossPercent = discord.ui.TextInput(
+        label = '最大回撤',
+        placeholder = '可承受之最大回撤（％）'
+    )
+    month = discord.ui.TextInput(
+        label = '試算時間段',
+        placeholder = '輸入『西元年-月份』ex:2022-09'
+    )
+
+    async def on_submit(self, interaction: discord.Interaction):
+        # print(self.traderLink)
+        resData = await util.analyzeTraderMDD(str(self.traderLink).split('?id=')[1], int(str(self.initialCapital)), int(str(self.maxLossPercent)), str(self.month))
+        # print(resData)
+        # print("交易員：" + resData["traderName"])
+        embed = discord.Embed(
+            title = str(self.month) + "月風險試算",
+            type = "rich",
+            description = f'交易員: {resData["traderName"]}'
+        )\
+        .add_field(name=" ", value=" ", inline=False)\
+        .add_field(name="本金", value=f'{ str(self.initialCapital) }USDT')\
+        .add_field(name="設定最大回撤", value=f'{ str(self.maxLossPercent) }％')\
+        .add_field(name="試算時間段", value=f'{ str(self.month) }月')\
+        .add_field(name=" ", value=" ", inline=False)\
+        .add_field(name="最大回合持倉數", value=f'{ resData["maxPosition"] }')\
+        .add_field(name="最大浮動虧損 (倉)", value=f'{ resData["drawdownHighest"] }倍倉')\
+        .add_field(name="最大浮動虧損 (％)", value=f'{ resData["drawdownHighestPercent"] }％')\
+        .add_field(name=" ", value=" ", inline=False)\
+        .add_field(name="發生最大浮動虧損時持倉數", value=f'{ resData["drawdownHighestPosition"] }')\
+        .add_field(name="潛在極端浮動虧損 (倉)", value=f'{ resData["potentialMDD"] }倍倉')\
+        .add_field(name="潛在極端浮動虧損 (％)", value=f'{ resData["potentialMDDPercent"] }％')\
+        .add_field(name=" ", value=" ", inline=False)\
+        .add_field(name="風報比平均值", value=f'{ resData["RDMean"] }')\
+        .add_field(name="風報比標準差", value=f'{ resData["RDDev"] }')\
+        .add_field(name="風報比中位數", value=f'{ resData["RDMid"] }')\
+        .add_field(name=" ", value=" ", inline=False)\
+        .add_field(name="建議安全保證金", value=f'{ resData["safeMargin"] }')\
+        .add_field(name="建議安全倉數上限", value=f'{ resData["positionEstimate"] }')\
+        .add_field(name=" ", value=" ")\
+        .add_field(name="※潛在極端", value="將前 N 大單筆保證金浮動虧損組合進最大回合持倉內計算", inline=False)\
+        .add_field(name="※建議安全保證金", value="(最大可接受虧損金額 ÷ 最大保證金浮動虧損 (％)) ÷ 最大保證金浮動虧損時持倉數", inline=False)\
+        .add_field(name="※建議安全總倉數", value="持倉數平均值 + 2 × 持倉數標準差 (包含 95% 事件)", inline=False)\
+        .add_field(name="※風報比", value="收益 ÷ 最大浮動虧損", inline=False)
+        await interaction.response.send_message(embed = embed, ephemeral = True)
+
+    async def on_error(self, interaction: discord.Interaction, error: Exception) -> None:
+        await interaction.response.send_message('Oops! Something went wrong.', ephemeral = True)
+        # Make sure we know what the error actually is
+        traceback.print_exception(type(error), error, error.__traceback__)
+        # send error message to bot log channel
+        botLogChannel = self.get_channel(944846007051620404)
+        botLogChannel.send(f'{type(error)}{error}{error.__traceback__}')
+
+class CopyBot(discord.Client):
+    def __init__(self):
+        intents = discord.Intents.default()
+        intents.members = True
+        intents.message_content = True
+        intents.guilds = True
+        super().__init__(command_prefix=commands.when_mentioned_or('/'), intents=intents)
+        self.tree = app_commands.CommandTree(self)
+
+    async def setup_hook(self) -> None:
+        await self.tree.sync(guild = KNS)
+        self.add_view(menuView())
+
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        mainChannel = self.get_channel(914526949630693458)
+        botLogChannel = self.get_channel(944846007051620404)
+        # msg = await channel.fetch_message(1085137357273043055)
+        # await msg.edit(content="What's your favourite colour?", view=PersistentView())
+        await mainChannel.send("Bitget 交易員分析 & 試算工具", view=menuView())
+        print('ready------')
+
+bot = CopyBot()
+
+
+@bot.tree.command(name = "ping", description = "pong!", guild = KNS)
 async def ping(ctx):
     await ctx.response.send_message("pong!")
-
-@tree.command(name = "weekreport", description = "get bunch of best traders this week!", guild = KNS)
-async def weekreport(ctx, uid: str):
-    await ctx.response.send_message("We're working on it...")
-
-@tree.command(name = "performance", description = "Show performance of specific trader", guild = KNS)
-async def performance(ctx, uid: str):
-    resData = util.get_trader_drawdown(uid)
-    await ctx.response.send_message("traderName:\n"+ resData['traderName'] + "\nlast month PNL:\n" + str(resData['pnlInDays']))
-
-
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
-    await tree.sync(guild=discord.Object(id = guild_id))
-    print('------')
-
 
 
 bot.run(token)
--- util.py
+++ util.py
@@ -1,6 +1,5 @@
 import os
 from dotenv import load_dotenv
-import config
 import math
 import time
 import random
@@ -9,79 +8,316 @@ from time import sleep
 from datetime import datetime, timezone, timedelta
 import pymongo
 import statistics
+import tqdm
 
+monthMap = {
+    "01": "31",
+    "02": "28",
+    "03": "31",
+    "04": "30",
+    "05": "31",
+    "06": "30",
+    "07": "31",
+    "08": "31",
+    "09": "30",
+    "10": "31",
+    "11": "30",
+    "12": "31",
+}
 
-def get_trader_drawdown(traderId):
+async def copySimulate(traderId, margin = 10, lossPerPosition = 100, month = "2023-02"):
     load_dotenv()
     client = pymongo.MongoClient(os.getenv('MONGODB'))
     drawdownDB = client.TraderDrawdown.traderDrawdown
-
-    # traderDrawdown Schema EXAMPLE:
-    # _id: 639e8524dbfdd377db639603
-    # traderId: "5938058384"
-    # state: "active"
-    # updateDate: "2023-01-12 04:09:48"
-    # traderName: "紫渝"
-    # ROI: -32.46
-    # totalTrade: 271
-    # winTrade: 188
-    # loseTrade: 83
-    # totalFollower: 205
-    # totalRevenue: 176.58
-    # history: [   
-        # side: "short"
-        # leverage: 50
-        # symbol: "ETHUSDT"
-        # marginType: "cross"
-        # openDate: "2023-01-07 12:16:37"
-        # openPrice: 1265.63
-        # closeDate: "2023-01-11 23:13:28"
-        # closePrice: 1333.2
-        # revenue: -266.94
-        # orderId: "995472239410921475"
-        # drawdown: 324.19
-    # ]
-    # follower: 125
+    lastday = monthMap[month.split('-')[1]]
 
     traderData = drawdownDB.find({"traderId": traderId})
     traderData = (list(traderData)[0])
     # print(traderData)
     if len(traderData) == 0 :
         hasDrawdownData = False
-        print('Cannot find trader data in DB.')
+        # print('Cannot find trader data in DB.')
+        return False
     else:
-        dayCount = 28
-        margin = 10
-        pnlInDays = np.zeros(dayCount, dtype=float)
-        startTimestamp = int(time.mktime(time.strptime("2023-02-01 00:00:00", "%Y-%m-%d %H:%M:%S")))
-        endTimestamp = int(time.mktime(time.strptime("2023-02-28 23:59:59", "%Y-%m-%d %H:%M:%S")))
+        positionCount = 0
+        suggestCapital = 0.00
+        # margin = 10
+        pnlInDays = np.zeros(int(lastday), dtype=float)
+        startTimestamp = int(time.mktime(time.strptime(f"{month}-01 00:00:00", "%Y-%m-%d %H:%M:%S"))) - 3600*8
+        endTimestamp = int(time.mktime(time.strptime(f"{month}-{lastday} 23:59:59", "%Y-%m-%d %H:%M:%S"))) - 3600*8
         traderShareRatio = 0.1
-        totalTraderShare = 0.0
+        totalTraderShare = 0.00
+        copyProfit = 0.00
         hasDrawdownData = True
         history = traderData["history"]
         traderName = traderData["traderName"]
+        countSL = 0
+
         for each in history:
             openTimestamp = int(time.mktime(time.strptime(each["openDate"], "%Y-%m-%d %H:%M:%S")))
             closeTimestamp = int(time.mktime(time.strptime(each["closeDate"], "%Y-%m-%d %H:%M:%S")))
             leverage = each["leverage"]
+            drawdown = each["drawdown"]
             if startTimestamp <= openTimestamp and closeTimestamp <= endTimestamp:
+                positionCount += 1
                 day = int((closeTimestamp-startTimestamp)/86400)
                 # 手續費6%, 開倉平倉各付一次
                 fee = 0.0006 * margin * leverage * 2
-                pnl = (margin * float(each["revenue"])/100) - fee
+                if drawdown >= lossPerPosition :
+                    pnl = -1 * margin
+                    countSL += 1
+                else:
+                    pnl = (margin * float(each["revenue"])/100) - fee
 
                 if pnl > 0:
                     pnlInDays[day] += pnl * (1 - traderShareRatio)
                     totalTraderShare += pnl * traderShareRatio
+                    copyProfit += pnl * (1 - traderShareRatio)
                 else:
                     pnlInDays[day] += pnl
+                    copyProfit += pnl
 
         return {
-            "traderName": traderName,
-            "pnlInDays": pnlInDays,
-            "totalTraderShare": totalTraderShare
+            "traderName": traderName,               # 交易員名稱
+            "positionCount": positionCount,         # 總計開倉數
+            "pnlInDays": pnlInDays,                 # 每日收益
+            "totalTraderShare": totalTraderShare,   # 帶單員分潤收益
+            "copyProfit": round(copyProfit, 2),     # 跟單收益
+            "suggestCapital": suggestCapital,       # 建議本金
+            "countSL": countSL,                     # 共計止損？次
         }
+    
+
+def potentialExtremeMaxDrawdown(drawdownData, turns):
+    drawdownList = []
+    roundList = []
+    potentialMaxDrawdown = 0
+
+    for r in range(len(turns)):
+        roundList.append(turns[r][0])
+
+    for row in drawdownData:
+        drawdownList.append(round(float(row["drawdown"]) / 100, 2))
+
+    while (len(drawdownList) < sum(roundList)):
+        drawdownList.append(
+            drawdownList[random.randint(0, len(drawdownList) - 1)])
+
+    maxPosition = max(roundList)
+    drawdownList.sort(reverse=True)
+    for i in range(maxPosition):
+        potentialMaxDrawdown = potentialMaxDrawdown + drawdownList[i]
+
+    return potentialMaxDrawdown
+
+
+async def analyzeTraderMDD(traderId, initialCapital = 10000, maxLossPercent = 20, month = "2023-02"):
+    # Initialize
+    load_dotenv()
+    client = pymongo.MongoClient(os.getenv('MONGODB'))
+    drawdownDB = client.TraderDrawdown.traderDrawdown
+    lastday = monthMap[month.split('-')[1]]
+
+    hasDrawdownData = False
+
+    # Query in drawdown DB
+    traderData = drawdownDB.find({"traderId": traderId})
+    traderData = (list(traderData)[0])
+    # print(traderData)
+    if len(traderData) == 0 :
+        hasDrawdownData = False
+        # print('Cannot find trader data in DB.')
+        return False
+    else:
+        hasDrawdownData = True
+        startTimestamp = int(time.mktime(time.strptime(f"{month}-01 00:00:00", "%Y-%m-%d %H:%M:%S"))) - 3600*8
+        endTimestamp = int(time.mktime(time.strptime(f"{month}-{lastday} 23:59:59", "%Y-%m-%d %H:%M:%S"))) - 3600*8
+        history = traderData["history"]
+        traderName = traderData["traderName"]
+        drawdownData = []
+        for each in history:
+            openTimestamp = int(time.mktime(time.strptime(each["openDate"], "%Y-%m-%d %H:%M:%S")))
+            closeTimestamp = int(time.mktime(time.strptime(each["closeDate"], "%Y-%m-%d %H:%M:%S")))
+            if startTimestamp <= openTimestamp and closeTimestamp <= endTimestamp:
+                drawdownData.append(each)
+
+        # Round
+        turns = []
+        operating = []
+        RevenueVSDrawback = []
+        if hasDrawdownData == True:
+            for row in drawdownData:
+                remove = []  # 存放要刪除的筆數
+                # ---more data--
+                if row["drawdown"] <= 0: # 無回撤通常伴隨小收益，負回撤也歸類到1
+                    RevenueVSDrawback.append(1)
+                else:
+                    RevenueVSDrawback.append(row["revenue"] / row["drawdown"])
+                # --------------
+
+                for trade in operating:
+                    # 新 trade 與在場上的 trade 時間不重疊
+                    if time.mktime(time.strptime(row["closeDate"], "%Y-%m-%d %H:%M:%S")) < time.mktime(time.strptime(trade["openDate"], "%Y-%m-%d %H:%M:%S")):
+                        remove.append(trade)
+                if len(remove) != 0:
+                    draw = 0.0
+                    num = 0
+                    for trade in operating:
+                        num = num + 1
+                        draw = draw + trade["drawdown"]
+                    turns.append([num, round(draw / 100, 2)])
+                    for re in remove:
+                        operating.remove(re)
+                operating.append(row)
+            # 最後一筆資料
+            draw = 0.0
+            num = 0
+            for trade in operating:
+                num = num + 1
+                draw = draw + trade["drawdown"]
+            turns.append([num, round(draw / 100, 2)])
+
+            # Result
+            # print(turns)
+            if len(turns) != 0:
+                # 最大回撤保證金(倉數) 回撤保證金倉數平均、 標準差
+                maxPosition = max([(a[0]) for a in turns])
+                drawdownHighestPosition = int(turns[np.argmax(turns, axis=0)[1]][0])
+                drawdownHighest = np.max(np.array(turns), axis=0)[1]
+                drawdownMean = statistics.mean((a[1]) for a in turns)
+                drawdownDev = statistics.pstdev((a[1]) for a in turns)
+                drawdownEstimate = drawdownMean + (3 * drawdownDev)
+                positionMean = statistics.mean((a[0]) for a in turns)
+                positionDev = statistics.pstdev((a[0]) for a in turns)
+                positionEstimate = str(round(positionMean + (2 * positionDev), 0)).split(".")[0]
+
+                # 收益/最大浮動虧損
+                #RD = statistics.NormalDist.from_samples(RevenueVSDrawback)
+                RDMean = statistics.mean(RevenueVSDrawback)
+                RDDev = statistics.pstdev(RevenueVSDrawback)
+                # RDmax = max(RevenueVSDrawback)
+                RDQuantiles = []
+                RevenueVSDrawback.sort()
+                for i in range(len(RevenueVSDrawback)):
+                    for a in range(0, 11):
+                        if i == int((len(RevenueVSDrawback) - 1) * a / 10):
+                            RDQuantiles.append(round(RevenueVSDrawback[i], 2))
+
+                # Monte Carlo simulation
+                # monteCarloMaxDrawdown, monteCarloPosition = monteCarloSimulation(
+                #     drawdownData, turns)
+
+                # Potential extreme maximum drawdown
+                potentialMaxDrawdown = potentialExtremeMaxDrawdown(drawdownData, turns)
+
+                # Position strategy
+                maxLoss = initialCapital * (maxLossPercent / 100)
+                strategyMaxDrawdown = max(drawdownHighest, drawdownEstimate)
+                safeTotalMargin = maxLoss / (strategyMaxDrawdown / drawdownHighestPosition)
+                safeMargin = round(safeTotalMargin / drawdownHighestPosition, 2)
+
+
+                # print(f"※常態分佈 = 保證金浮動虧損平均值 + 3 × 保證金浮動虧損標準差 (包含 99.73% 事件)")
+                # print(f"※潛在極端 = 將前 N 大單筆保證金浮動虧損組合進最大回合持倉內計算")
+                # print(f"※建議安全保證金 = (最大可接受虧損金額 ÷ 最大保證金浮動虧損 (％)) ÷ 最大保證金浮動虧損時持倉數")
+                # print(f"※建議安全總倉數 = 持倉數平均值 + 2 × 持倉數標準差 (包含 95% 事件)\n\n\n")
+                # print(f"※風報比 = 收益 ÷ 最大浮動虧損")
+                
+                return {
+                    "traderName": traderName,
+                    "traderId": traderId,
+                    "maxPosition": maxPosition,             # 最大回合持倉數
+                    "drawdownHighest": drawdownHighest,     # 最大保證金浮動虧損 (倉)
+                    "drawdownHighestPercent": round((drawdownHighest / drawdownHighestPosition) * 100, 2),  # 最大保證金浮動虧損 (％)
+                    "drawdownHighestPosition": drawdownHighestPosition,     # 發生最大保證金浮動虧損時持倉數
+                    "potentialMDD": round(potentialMaxDrawdown, 2),         # 潛在極端最大保證金浮動虧損 (倉)
+                    "potentialMDDPercent": round((potentialMaxDrawdown / maxPosition) * 100, 2),    # 潛在極端最大保證金浮動虧損 (％)
+                    "RDMean": round(RDMean, 2),             # 風報比平均值
+                    "RDDev": round(RDDev, 2),               # 風報比標準差
+                    "RDMid": RDQuantiles[5],                # 風報比中位數
+                    "initialCapital": initialCapital,       # 初始資金（使用者設定）
+                    "maxLossPercent": maxLossPercent,       # 最大風險（使用者設定）
+                    "safeMargin": safeMargin,               # 建議安全保證金
+                    "positionEstimate": positionEstimate,   # 建議安全倉數上限
+                }
+
+
+# My handmade function: FAILED
+def calcMaxMDD(traderId):
+    load_dotenv()
+    client = pymongo.MongoClient(os.getenv('MONGODB'))
+    drawdownDB = client.TraderDrawdown.traderDrawdown
+
+    traderData = drawdownDB.find({"traderId": traderId})
+    traderData = (list(traderData)[0])
+    # print(traderData)
+    if len(traderData) == 0 :
+        hasDrawdownData = False
+        # print('Cannot find trader data in DB.')
+        return False
+    else:
+        positionCount = 0
+        positions = []
+        suggestCapital = 0.00
+        startTimestamp = int(time.mktime(time.strptime(f"2023-02-20 00:00:00", "%Y-%m-%d %H:%M:%S"))) + 3600*8
+        endTimestamp = int(time.mktime(time.strptime(f"2023-03-10 23:59:59", "%Y-%m-%d %H:%M:%S"))) + 3600*8
+        hasDrawdownData = True
+        history = traderData["history"]
+
+        for each in history:
+            openTimestamp = int(time.mktime(time.strptime(each["openDate"], "%Y-%m-%d %H:%M:%S")))
+            closeTimestamp = int(time.mktime(time.strptime(each["closeDate"], "%Y-%m-%d %H:%M:%S")))
+            leverage = each["leverage"]
+            drawdown = each["drawdown"]
+            if startTimestamp <= openTimestamp and closeTimestamp <= endTimestamp:
+                # print(f'{openTimestamp}, {closeTimestamp}')
+                positions.append({
+                    "openTimestamp": openTimestamp,
+                    "closeTimestamp": closeTimestamp,
+                    "drawdown": drawdown,
+                    "overlapPos": []
+                })
+
+        overlap = np.zeros((len(positions), len(positions)), dtype="int_")
+        for i in range(len(positions)):
+            for j in range(len(positions)):
+                if positions[i]["openTimestamp"] < positions[j]["closeTimestamp"] :
+                    overlap[i][j] = 1
+                else :
+                    overlap[i][j] = 0
+
+        for i in range(len(positions)):
+            for j in range(len(positions)):
+                if overlap[i][j] != overlap[j][i]:
+                    overlap[i][j] = 0
+                    overlap[j][i] = 0
+                elif overlap[i][j] == 1 and overlap[j][i] == 1:
+                    positions[i]["overlapPos"].append(j)
+                    pass
+
+        print(positions[64])
+        # print(overlap)
+        print(positions[256])
+
+        MDD = 0
+        for eachPos in positions :
+            for eachOverlap in eachPos["overlapPos"]:
+                MDD += positions[eachOverlap]["drawdown"]
+            eachPos["overlapMDD"] = MDD
+            MDD = 0
+
+        # for i, eachPos in enumerate(positions) :
+        #     print(f'{i}  {eachPos["overlapPos"]}')
+        #     pass
+
+        # for i in range(len(positions)-200):
+            # for j in range(len(positions)-200):
+                # print(overlap[i][j], end=",")
+            # print('\n')
+
+        return 0
 
 
 if __name__ == "__main__":
-    get_trader_drawdown("2442813206")
+    # copySimulate("2442813206")
+    print(analyzeTraderMDD("4876423973", 10000, 10, "2023-02"))
