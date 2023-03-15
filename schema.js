0d201ad771d34f0a82e23d41ca379191137cd3e2
mchu-1999 <jerry.ee06@nctu.edu.tw>
2023-03-15 16:17:33 +0800

    DB schema

---
+++ schema.js
@@ -0,0 +1,28 @@
+// traderDrawdown Schema:
+EXAMPLE = {
+    _id: "639e8524dbfdd377db639603",
+    traderId: "5938058384",
+    state: "active",
+    updateDate: "2023-01-12 04:09:48",
+    traderName: "紫渝",
+    ROI: -32.46,
+    totalTrade: 271,
+    winTrade: 188,
+    loseTrade: 83,
+    totalFollower: 205,
+    totalRevenue: 176.58,
+    history: {
+        side: "short",
+        leverage: 50,
+        symbol: "ETHUSDT",
+        marginType: "cross",
+        openDate: "2023-01-07 12:16:37",
+        openPrice: 1265.63,
+        closeDate: "2023-01-11 23:13:28",
+        closePrice: 1333.2,
+        revenue: -266.94,
+        orderId: "995472239410921475",
+        drawdown: 324.19
+    },
+    follower: 125,
+}
