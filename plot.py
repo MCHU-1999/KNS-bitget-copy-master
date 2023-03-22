27f806bab3a2687744c2282c56d6e5c436594041
mchu-1999 <jerry.ee06@nctu.edu.tw>
2023-03-22 11:02:07 +0800

    search from date to today

--- plot.py
+++ plot.py
@@ -8,28 +8,29 @@ def getTime():
     dt2 = dt1.astimezone(timezone(timedelta(hours=8)))
     return dt2.strftime("%Y-%m-%d-%H-%M-%S")
 
-def plotLine(x, y, month:str):
+def plotLine(x, y, startDate:str, endDate:str):
     try:
         timeNow = getTime()
-        date = [month + '/' + str(each) for each in x]
+        # date = [month + '/' + str(each) for each in x]
         yLast = [y[-1] for each in y]
         plt.plot(x, y, linewidth=2, solid_capstyle='round', c='YellowGreen')  # Plot some data on the axes.
+        plt.grid(color='silver', linestyle='--', linewidth=0.5)
         plt.plot(x, yLast, linestyle='--', linewidth=1, solid_capstyle='round', c='grey')  # final funds
         plt.plot(x[-1], y[-1], marker='o', c='YellowGreen')
         plt.text(x[-1]+0.5, y[-1], f'{ round(y[-1],2) }\nUSDT', c='black')
         plt.xticks(
-            ticks = x,
-            labels = date,
+            ticks = [x[0],x[-1]],
+            labels = [startDate, endDate],
             color = 'grey',
-            fontsize = 8,
-            rotation = 45
+            fontsize = 12
+            # rotation = 45
         )
-        plt.grid(color='silver', linestyle='--', linewidth=0.5)
         plt.xlabel('Date',{'fontsize':12,'color':'black'})
         plt.ylabel('Fund (USDT)',{'fontsize':12,'color':'black'})
         # plt.show()
         plt.savefig(f'./{ timeNow }.png')
         plt.close('all')
+        print(f'genegate: { timeNow }.png')
         return f'{ timeNow }.png'
 
     except Exception as e:
